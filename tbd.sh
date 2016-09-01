8dfd4a779f26c44c0d293369a552e4d684ba6745
waltermblair <wmblair@cofc.edu>
2016-09-01 14:41:27 -0400

    Second stab includes editing with file with sed

--- tbd.sh
+++ tbd.sh
@@ -3,11 +3,14 @@
 
 # Send list of top-level directory contents of containing directory to new html file
 # Sources: http://www.rapidtables.com/code/linux/ls.htm
-# https://www.gnu.org/software/sed/manual/sed.html
-# http://www.computerhope.com/unix/used.htm
 ls -x -lh > "list.html" 
 
 # Modify html file with <html> and other tags
+# https://www.digitalocean.com/community/tutorials/the-basics-of-using-the-sed-stream-editor-to-manipulate-text-in-linux
+sed -i '1 i<html>' list.html
+sed -i '$ a</html>' list.html
+sed -i '1~2a<p>' list.html
+sed -i '2~1a</p>' list.html
 
 # Open file in browser
 # Sources: http://stackoverflow.com/questions/3124556/clean-way-to-launch-the-web-browser-from-shell-script
