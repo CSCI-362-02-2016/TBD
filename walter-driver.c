34101af64dfeab89d7cb7c14b16d589b4c98a49b
waltermblair <wmblair@cofc.edu>
2016-09-27 06:40:03 -0400

    Step toward running buddyicon.c, I get missing dependencies here, perhaps can solve by compiling all of them?

---
+++ walter-driver.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "libpurple/buddyicon.c"
+
+void main(void) {
+	hello();
+	return;
+}
---
+++ walter-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#  Compiles test-driver.c and buddyiconf.c */
+#  glib header files were tricky: http://stackoverflow.com/questions/1146010/why-cant-i-build-a-hello-world-for-glib and https://developer.gnome.org/glib/stable/glib-compiling.html
+
+# http://stackoverflow.com/questions/29998485/in-c-programming-what-is-undefined-referenceerror-when-compiling
+
+cc pidgin-eclipse/libpurple/buddyicon.c pidgin-eclipse/walter-driver.c `pkg-config --cflags --libs glib-2.0` -o "walter-driver" 
+# cc pidgin-eclipse/walter-driver.c `pkg-config --cflags --libs glib-2.0` -o "walter-driver"
+
+
+#  Then calls test-driver binary */
+./pidgin-eclipse/walter-driver
