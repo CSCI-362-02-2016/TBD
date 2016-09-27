4ebd625ef3a3000a311db7ef07a26e797d70d461
waltermblair <wmblair@cofc.edu>
2016-09-27 09:31:43 -0400

    update

--- walter/walter-script.sh
+++ walter/walter-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#  Compiles test-driver.c and buddyiconf.c */
+#  Compiles walter-driver.c and buddyiconf.c */
 #  glib header files were tricky: http://stackoverflow.com/questions/1146010/why-cant-i-build-a-hello-world-for-glib and https://developer.gnome.org/glib/stable/glib-compiling.html
 
 # http://stackoverflow.com/questions/29998485/in-c-programming-what-is-undefined-referenceerror-when-compiling
