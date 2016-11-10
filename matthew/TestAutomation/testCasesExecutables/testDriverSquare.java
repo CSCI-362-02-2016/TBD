2922431cc4300243b4638f33d3ef88d37cbd32f6
waltermblair <wmblair@cofc.edu>
2016-11-10 12:29:59 -0500

    updated square driver

--- matthew/TestAutomation/testCasesExecutables/testDriverSquare.java
+++ matthew/TestAutomation/testCasesExecutables/testDriverSquare.java
@@ -5,10 +5,9 @@ class testDriverSquare {
 	public static void main(String[] args) {
 		// Instance of Calculator class
 		Calculator calc = new Calculator();
-
-		// args is input, test that args contains these
+		Double num = Double.parseDouble(args[0]);
 		try {
-			return calc.calculateMono(Calculator.MonoOperatorModes.square, args[0]);
+			System.out.println(calc.calculateMono(Calculator.MonoOperatorModes.square, num));
 		}
 		catch(Exception e) {
 			System.out.println("Need command-line args!");
